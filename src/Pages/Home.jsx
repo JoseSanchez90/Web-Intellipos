1100d878316ea539dd1e69cc700c4b9dab9fa39e
Jose Sanchez <josesancheztrujillo90@hotmail.com>
2024-12-13 15:29:44 -0500

    update btn-contact

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -27,10 +27,11 @@ function Home() {
     <section className="w-full h-full outfit-letters">
 
     <div className="relative h-full bg-cover" style={{ backgroundImage: `url(${fondo1})` }}>
-      <div className= {`sticky top-0 z-10 flex flex-row justify-between items-center bg-white px-10 py-5 transition-all duration-300 ${
-        isScrolled ? "shadow-lg rounded-xl bg-white px-20 py-4 top-4 w-[98%] left-4 text-sm" : "sticky top-0 z-10 flex flex-row justify-between items-center bg-white px-10 py-5"}`}>
+
+      <div className= {`sticky top-0 z-10 flex flex-row justify-between items-center bg-white px-10 py-6 transition-all duration-300 ${
+        isScrolled ? "shadow-lg rounded-b-3xl bg-white px-20 py-4 text-sm" : ""}`}>
         <div>
-          <img src={logoInt} alt={logoInt} className={`w-48 transition-all duration-300 ${isScrolled ? "w-32" : ""}`} />
+          <img src={logoInt} alt={logoInt} className={`w-48 transition-all duration-300 ${isScrolled ? "w-28" : ""}`} />
         </div>
         <div className="font-semibold flex gap-12 items-center">
           <a href="" className="relative group">Inicio
@@ -48,14 +49,14 @@ function Home() {
           <a href="" className="relative group">Clientes
           <span className="absolute left-0 bottom-0 w-0 h-1 bg-green-500 transition-all duration-300 group-hover:w-full"></span>
           </a>
-          <a href="" className="bg-green-600 px-5 py-2 rounded-lg text-white">Contactanos</a>
+          <a href="" className="contact"><span className="contact-content">Contactanos</span></a>
         </div>
       </div>
-      
+
       <div className="absolute inset-0 bg-black/50"></div>
         
         <div className="h-screen w-full">
-          <div className="py-32 px-96 right-72 relative">
+          <div className="py-32 pr-[45%] ml-16 relative">
             <p className="text-green-100 font-semibold text-5xl pb-6">Llevamos la adminsitracion de tu restaurante al siguiente nivel</p>
             <p className="text-green-100 font-normal text-xl pb-8">El manejo de tu restaurante no tiene por qué ser difícil, cuando cuentas con una herramienta eficiente, escalable y fácil de usar, lo que resultará en más clientes satisfechos y leales.</p>
             <button className="info">
